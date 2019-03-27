badc3ce9f1cfa3369fee8641d16ad4d67be2dd68
igordefo <dekleidung@gmail.com>
2019-03-27 20:51:21 +0200

    Learn variable mutation and type coercion

--- js-basics/script.js
+++ js-basics/script.js
@@ -2,21 +2,45 @@
 * Variables and data types
 */
 
-console.log('Hello World');
+// console.log('Hello World');
+
+// var firstName = 'Igor';
+// var lastName = 'Posunko';
+// var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// age = 29;
+// fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// job = 'worker';
+// console.log(job);
+
+// var $ffdf = 29;
+// var _3dfd = 4;
+// var if = 23;
+
+/********************************
+* Variable mutation and type coercion
+*/
 
 var firstName = 'Igor';
-var lastName = 'Posunko';
-var fullName = firstName + ' ' + lastName;
-console.log(fullName);
+var age = 29;
+// Type coercion
+console.log(firstName + ' ' + age);
 
-age = 29;
-fullAge = true;
-console.log(fullAge);
+var job, isMarried;
+job = 'driver';
+age = 'twenty eight';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var job;
+// Variable mutation
+
 job = 'worker';
-console.log(job);
+age  = 29;
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var $ffdf = 29;
-var _3dfd = 4;
-var if = 23;
+var lastName = prompt('What is his last name?')
+console.log(firstName + ' ' + lastName);
