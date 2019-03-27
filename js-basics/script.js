946a840f8dd17e155415b6b4bec8fb5d11706e89
igordefo <dekleidung@gmail.com>
2019-03-27 19:17:07 +0200

    Learn variables and data types

---
+++ js-basics/script.js
@@ -0,0 +1,22 @@
+/********************************
+* Variables and data types
+*/
+
+console.log('Hello World');
+
+var firstName = 'Igor';
+var lastName = 'Posunko';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+age = 29;
+fullAge = true;
+console.log(fullAge);
+
+var job;
+job = 'worker';
+console.log(job);
+
+var $ffdf = 29;
+var _3dfd = 4;
+var if = 23;
