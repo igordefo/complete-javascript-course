adf377666debca33f3847acc708a837b2a031094
igordefo <dekleidung@gmail.com>
2019-03-28 19:08:08 +0200

    Learn basic operators

--- js-basics/script.js
+++ js-basics/script.js
@@ -1,30 +1,31 @@
 /********************************
 * Variables and data types
 */
+/*
+console.log('Hello World');
 
-// console.log('Hello World');
+var firstName = 'Igor';
+var lastName = 'Posunko';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
 
-// var firstName = 'Igor';
-// var lastName = 'Posunko';
-// var fullName = firstName + ' ' + lastName;
-// console.log(fullName);
+age = 29;
+fullAge = true;
+console.log(fullAge);
 
-// age = 29;
-// fullAge = true;
-// console.log(fullAge);
+var job;
+job = 'worker';
+console.log(job);
 
-// var job;
-// job = 'worker';
-// console.log(job);
-
-// var $ffdf = 29;
-// var _3dfd = 4;
-// var if = 23;
+var $ffdf = 29;
+var _3dfd = 4;
+var if = 23;
+*/
 
 /********************************
 * Variable mutation and type coercion
 */
-
+/*
 var firstName = 'Igor';
 var age = 29;
 // Type coercion
@@ -44,3 +45,35 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last name?')
 console.log(firstName + ' ' + lastName);
+*/
+
+/********************************
+* Basic operators
+*/
+/*
+var now, yearIgor, yearJohn;
+now = 2019;
+ageIgor = 29;
+ageJohn = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearIgor = now - ageIgor;
+console.log(yearIgor);
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+// Logical operators
+var igorOlder = ageIgor < ageJohn;
+console.log(igorOlder);
+
+// typeof operator
+console.log(typeof(ageJohn));
+console.log(typeof(igorOlder));
+console.log(typeof('Igor is awesome'));
+var x;
+console.log(typeof(x));
+*/
