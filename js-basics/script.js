4c3adf9f51a52622db4c56b07082b9003a794180
igordefo <dekleidung@gmail.com>
2019-03-31 11:53:45 +0300

    Solve coding challenge #2

--- js-basics/script.js
+++ js-basics/script.js
@@ -253,3 +253,49 @@ if(height == 0) {
   console.log('Operator == does type coercion');
 }
 
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, 
+while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average 
+score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same 
+  average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the 
+console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, 
+it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+var avgJohnTeam = (300 + 94 + 123) / 3;
+var avgMikeTeam = (500 + 94 + 123) / 3;
+var avgMary = (500 + 134 + 105) / 3;
+
+if (avgJohnTeam > avgMikeTeam) {
+  console.log('John\'s team has highest average score');
+} else if (avgMikeTeam > avgJohnTeam) {
+  console.log('Mike\'s team has highest average score');
+} else {
+  console.log('Draw');
+}
+
+switch(true) {
+  case avgJohnTeam > avgMikeTeam && avgJohnTeam > avgMary: 
+  console.log('John\'s team has highest average score')
+  break
+  case avgMikeTeam > avgJohnTeam && avgMikeTeam > avgMary: 
+  console.log('Mike\'s team has highest average score');
+  break
+  case avgMary > avgJohnTeam && avgMary > avgMikeTeam: 
+  console.log('Mary\'s team has highest average score');
+  break
+  default: console.log('Draw');
+}
