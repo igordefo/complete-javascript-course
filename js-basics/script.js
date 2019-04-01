09a46d93a1d161fc87907dcb27eaff69f3cf0ffd
igordefo <dekleidung@gmail.com>
2019-04-01 22:52:23 +0300

    Learn arrays

--- js-basics/script.js
+++ js-basics/script.js
@@ -345,4 +345,72 @@ var whatDoYouDo = function(job, firstName) {
   }
 }
 
-console.log(whatDoYouDo('designer', 'Igor'))
+console.log(whatDoYouDo('designer', 'Igor'));
+
+/*****************************
+* Arrays
+*/
+
+var names = ['Igor', 'John', 'Jane'];
+var years = new Array(1990, 1988, 1984);
+
+console.log(names[0]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Lucy';
+console.log(names);
+
+// Different data types
+
+var user = ['Igor', 'Posunko', 1990, 'worker', true];
+user.push('blue');
+user.unshift('Mr.');
+
+user.pop();
+user.pop();
+user.shift();
+
+console.log(user);
+
+console.log(user.indexOf('worker'));
+var isWorker = user.indexOf('worker') === -1 ? 'Igor is not worker' : 'Igor is worker';
+console.log(isWorker);
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is 
+less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+function calculator(bill1) {
+
+  if(bill1 < 50) {
+    var tips1 = bill1 * 0.2;
+  } else if(bill1 >= 50 && bill1 <= 200) {
+    var tips1 = bill1 * 0.15;
+  } else {
+    var tips1 = bill1 * 0.1;
+  }
+
+
+  return tips1;
+}
+
+
+
+console.log(calculator(124));
