83774cad157e351b506397baeca48954263cd3b3
igordefo <dekleidung@gmail.com>
2019-04-10 22:37:55 +0300

    Updating Scores and Changing the Active Player

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -21,6 +21,11 @@ activePlayer = 0;
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -29,4 +34,25 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   var diceDOM = document.querySelector('.dice');
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
+
+  if(dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
 })
+
+
+function nextPlayer() {
+  roundScore = 0;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
