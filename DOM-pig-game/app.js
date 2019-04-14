dcad50e0be6d0448c51d389a468f6591ad4df7bb
igordefo <dekleidung@gmail.com>
2019-04-14 10:29:40 +0300

    Solve first part of coding challenge #6

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -9,9 +9,11 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice, gamePlaying;
+var scores, roundScore, activePlayer, dice, gamePlaying, sixInRow;
 
 init();
+
+var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if(gamePlaying) {
@@ -20,13 +22,20 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + ".png";
-  
-    if(dice !== 1) {
+
+    if (lastDice === 6 && dice === 6) {
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      nextPlayer();
+    } else if(dice !== 1) {
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  
     } else {
       nextPlayer();
     }
+
+    lastDice = dice;
   }
 })
 
@@ -84,3 +93,15 @@ function init() {
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 }
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
+ (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. 
+(Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. 
+(Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
