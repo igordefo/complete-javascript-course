7ba255f79b4fb0ddbdd457bcd966d41f39d76628
igordefo <dekleidung@gmail.com>
2019-04-14 10:33:47 +0300

    Solve second part of coding challenge #6

--- DOM-pig-game/app.js
+++ DOM-pig-game/app.js
@@ -45,7 +45,17 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 15) {
+    var value = document.querySelector('.final-score').value;
+    var winningScore;
+
+    if (value) {
+      winningScore = value;
+    } else {
+      winningScore = 100;
+    }
+
+
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
